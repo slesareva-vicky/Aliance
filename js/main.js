d549ff4e09b05940dff25c146a3cec2ef90bc3c6
slesareva-vicky <slesarevicky@gmail.com>
2023-10-21 12:35:39 +0300

    add: js переключение меню

--- js/main.js
+++ js/main.js
@@ -2,16 +2,36 @@ const navbar = document.querySelector(".navbar");
 const logo = document.querySelector(".logo-svg use");
 const mMenuToggle = document.querySelector (".mobile-menu-toggle");
 const menu= document.querySelector (".mobile-menu");
-window. addEventListener("scroll", () => {
-  if (this.scrollY > 1) {
-    navbar.classList.add("navbar-light");
-    logo.href.baseVal = "img/sprite.svg#logo";
-  } else {
-    navbar.classList.remove("navbar-light");
-    logo.href.baseVal = "img/sprite.svg#logo-light";
-  }
+
+const lightModeOn = () => { // Включаем светлый вариант меню
+  navbar.classList.add("navbar-light");
+  logo.href.baseVal = "img/sprite.svg#logo";
+}
+const lightModeOff = () => { // ВЫключаем светлый вариант меню
+  navbar.classList.remove("navbar-light");
+  logo.href.baseVal = "img/sprite.svg#logo-light";
+}
+const switchMode = () => {  //Переключатель вариантов меню 
+  window.scrollY > 1 ? lightModeOn() : lightModeOff();
+}
+const openMenu = () => { //Открытие меню 
+ menu.classList.add("is-open") //Подключаем стили открытого меню
+ mMenuToggle.classList.add("close-menu"); //Устанавливает стиль кнопки "крестик"
+ document.body.style.overflow = "hidden";  //запрет прокрутки документа
+ lightModeOn();
+}
+const closeMenu = () => { //Закрытие меню 
+  menu.classList.remove("is-open") //Отключаем стили открытого меню
+  mMenuToggle.classList.remove("close-menu"); //Восстанавливаем стиль кнопки "3 полоски"
+  document.body.style.overflow = "" ; //восстанавливаем по умолчанию прокрутку документа
+  switchMode();
+}
+
+window.addEventListener("scroll", () => {
+  switchMode();
 });
+
 mMenuToggle.addEventListener("click", (event) => {
-event.preventDefault();
-menu.classList.toggle("is-open")
+  event.preventDefault();
+  menu.classList.contains("is-open") ? closeMenu() : openMenu();
 })
