bf7bd5ac0ad8dbfc73c92c73ee0a35d9e1d99b8c
slesareva-vicky <slesarevicky@gmail.com>
2023-11-06 15:19:12 +0300

    fix: модальное окно

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ const logoLight = document.querySelector(".logo-light");
 const logo = document.querySelector(".logo");
 const mMenuToggle = document.querySelector(".mobile-menu-toggle");
 const menu = document.querySelector(".mobile-menu");
+const modal = document.querySelector(".modal");
+const modalDialog = document.querySelector(".modal-dialog");
 
 const swiper = new Swiper('.header-features-slider', {
   speed: 400,
@@ -116,18 +118,23 @@ mMenuToggle.addEventListener("click", (event) => {
   menu.classList.contains("is-open") ? closeMenu() : openMenu();
 })
 
-const modal = document.querySelector(".modal");
-const modalToggle = document.querySelectorAll("[data-toggle=modal]");
-const modalClose = document.querySelector(".modal-close")
-console.dir(modalToggle);
-modalToggle.forEach(element=> {
-  element.addEventListener("click", 
-  event=> {
+
+document.addEventListener("click", event => {
+  if (event.target.dataset.toggle == "modal" ||
+    event.target.parentNode.dataset.toggle == "modal" ||
+    (!event.composedPath().includes(modalDialog) &&
+      modal.classList.contains("is-open"))
+  ) {
     event.preventDefault();
-    modal.classList.add("is-open")
-  });
+    modal.classList.toggle("is-open");
+  }
 });
-modalClose.addEventListener("click", (event)=> {
- event.preventDefault();
- modal.classList.remove("is-open");
+
+document.addEventListener("keyup", event => {
+  if (event.key == "Escape" && modal.classList.contains("is-open")) {
+    event.preventDefault();
+    modal.classList.remove("is-open");
+  }
 });
+
+
